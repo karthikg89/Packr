c087dad00bf0c5094651be75843315d5eeef79fe
Karthik Gopalan <karthikg89@gmail.com>
2015-06-08 21:19:18 -0400

    Bypasses email authentication when default is activated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(valid_params)
     
-    if @user.save
+    if @user.save && !@user.activated?
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to login_url
